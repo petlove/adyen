6c48517fa70d5a16e84b9fbe5ced6a00fb58d445
Alex Borges <alex.borges@petlove.com.br>
2015-07-31 11:51:34 -0300

    Add refusal reason code to adyen response

--- lib/adyen/api/response.rb
+++ lib/adyen/api/response.rb
@@ -56,6 +56,47 @@ module Adyen
           message unless message.empty?
         end
       end
+
+      # @return [String,nil] The SOAP failure message code, if there is one.
+      def fault_code
+        @fault_code ||= ((extract_refusal_code_from_xml(xml_querier) || extract_refusal_code_from_refusal_reason) rescue nil)
+      end
+
+      def refusal_reason_raw
+        params.to_h[:additional_data].to_h['refusalReasonRaw']
+      end
+
+      def extract_refusal_code_from_refusal_reason
+        refusal_reason_raw.present? ? message_to_code(refusal_reason_raw) : nil
+      end
+
+      def message_to_code(msg)
+        I18n.t(msg, default: msg)
+      end
+
+      private
+
+      def namespaces
+        {
+          'ns1' => 'http://payment.services.adyen.com',
+          'default' => 'http://payment.services.adyen.com'
+        }
+      end
+
+      # Xpath vs namespace are not working for elements
+      def extract_refusal_code_from_xml(result)
+        body = result.xpath('//soap:Envelope/soap:Body').children.first
+        refusal_reason = body.xpath('//ns1:authoriseResponse/ns1:paymentResult/default:refusalReason/text()', namespaces).to_s
+        error_code_number(refusal_reason)
+      end
+
+      # So sad, I know
+      # Adyen API doesn't return a specific field with the error code
+      def error_code_number(refuse_message)
+        return nil if refuse_message.blank?
+        init = refuse_message.to_s.squish.split.first
+        !!(init =~ /^[0-9]{3}$/) ? init : nil
+      end
     end
   end
 end
